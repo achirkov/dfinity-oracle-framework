a9bfc286fea6d4476ab021a8ebeba00a9125260d
achirkov <mupinc@gmail.com>
2021-07-30 11:34:06 +0300

    Update dfxservice.go

--- dfxservice.go
+++ dfxservice.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hyplabs/dfinity-oracle-framework/models"
-	"github.com/hyplabs/dfinity-oracle-framework/utils"
+	"github.com/achirkov/dfinity-oracle-framework/models"
+	"github.com/achirkov/dfinity-oracle-framework/utils"
 	"github.com/sirupsen/logrus"
 )
 
